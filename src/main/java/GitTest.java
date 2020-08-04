398ffc107d2474a83b183c7b124247f7de1ccff6
lb7517 <lb_987@163.com>
2020-08-04 15:58:13 +0800

    添加222输出

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("创建了分支dev1-.1");
         System.out.println("111");
+        System.out.println("222");
     }
 }
