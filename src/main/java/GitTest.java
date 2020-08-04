3d274d9e53347836999aa2735e32817aaee0bc71
lb7517 <lb_987@163.com>
2020-08-04 16:48:53 +0800

    删除dev-1.1输出333

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -9,7 +9,6 @@ public class GitTest {
         System.out.println("111");
         System.out.println("222");
         System.out.println("分支111");
-        System.out.println("分支333");
         System.out.println("分支444");
     }
 }
